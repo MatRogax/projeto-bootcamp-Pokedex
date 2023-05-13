a49dd60e197c247005745812b086929a1511fd5c
Matheus Rogato <105300337+MatRogax@users.noreply.github.com>
2023-05-12 22:09:04 -0300

    .

---
+++ main.js
@@ -0,0 +1,13 @@
+const aval = 6.891
+const aval2 = 9.81
+
+
+
+const peso1 = 1.0
+const peso2 = 2.0
+
+const total = aval * peso1 + aval2 *peso2
+
+const media = total /(peso1 + peso2)
+
+console.log(media.toFixed(2))
